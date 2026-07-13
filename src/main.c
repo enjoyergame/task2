557774018e461f167af5ba3b295ea5dda5f9062b
Кучуков Ридаль <newnewfrfr@proton.me>
2026-07-13 21:00:50 +0500

    add main

--- src/main.c
+++ src/main.c
@@ -6,9 +6,66 @@ main.c
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "hexdump.h"
 
-int main(void)
+static void print_usage(const char *prog_name)
 {
-    printf("hexdump stub\n");
+    printf("Usage: %s [-i file] [-d directory] [-o offset] [-l size] [-g chunk_size] [-n chunk_count] [-f format]\n", prog_name);
+}
+
+int main(int argc, char *argv[])
+{
+    HexDumpConfig cfg = {
+        .file_name = NULL,
+        .dir_name = NULL,
+        .offset = 0,
+        .size = -1,
+        .chunk_size = 1,
+        .chunk_count = 16,
+        .format = NULL
+    };
+
+    int opt;
+    while ((opt = getopt(argc, argv, "i:o:l:g:n:d:f:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'i': cfg.file_name = optarg; break;
+            case 'd': cfg.dir_name = optarg; break;
+            case 'o': cfg.offset = strtol(optarg, NULL, 0); break;
+            case 'l': cfg.size = strtol(optarg, NULL, 0); break;
+            case 'g': cfg.chunk_size = atoi(optarg); break;
+            case 'n': cfg.chunk_count = atoi(optarg); break;
+            case 'f': cfg.format = optarg; break;
+            default:
+                print_usage(argv[0]);
+                return 1;
+        }
+    }
+
+    // Делегируем работу библиотеке
+    if (cfg.dir_name != NULL)
+    {
+        if (hexdump_process_dir(&cfg, stdout) != 0)
+        {
+            return 1;
+        }
+    }
+    else if (cfg.file_name != NULL)
+    {
+        if (hexdump_process(&cfg, stdout) != 0)
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Error: you must specify a file (-i) or directory (-d).\n");
+        print_usage(argv[0]);
+        return 1;
+    }
+
     return 0;
 }
